7cf2b4c647d3e64302108a1bbb2cf9fe222ee69a
Ashutosh <ashutoshgu18@gmail.com>
2019-05-17 16:40:13 +0530

    Add project files

---
+++ src/components/counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+    var classes = this.getBadgeClasses();
+
+    return (
+      <div>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    var classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+}
+
+export default Counter;
